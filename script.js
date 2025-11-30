54c20bc5a4a9290ff6c83c3d2ce5a2b61d6d08a2
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-11-30 16:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const byteSize = (str) => {
   // write your code here
- let count = 0;
-  for (let char of str) {
-    count++;
-  }
-  return count;
+  return new Blob([str]).size;
 };
 // Do not change the code below
 const str = prompt("Enter some string.");
