f1d9e0f33f94d51b4dc1055e21d98b1392a2c55d
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-11-30 16:02:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const byteSize = (str) => {
   // write your code here
- return str.length
+ let count = 0;
+  for (let char of str) {
+    count++;
+  }
+  return count;
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
